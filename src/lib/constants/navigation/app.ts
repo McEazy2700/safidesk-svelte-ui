0d012aeba7c9b852a0613c4e6038ea82c67b41eb
Ezekiel Victor <codeepoch@gmail.com>
2025-10-13 16:30:59 +0100

    Feat: update user navigation

--- src/lib/constants/navigation/app.ts
+++ src/lib/constants/navigation/app.ts
@@ -1,358 +1,360 @@
-import type { NavGroup } from '$lib/types/navigation';
-import BxsDashboard from '$lib/components/icons/bxs-dashboard.svelte';
-import Package2Outline from '$lib/components/icons/package-2-outline.svelte';
-import ComputerOutline from '$lib/components/icons/computer-outline.svelte';
-import Monitor from '$lib/components/icons/monitor.svelte';
-import Software from '$lib/components/icons/software.svelte';
-import NetworkLeft from '$lib/components/icons/network-left.svelte';
-import USB from '$lib/components/icons/usb.svelte';
-import Printer from '$lib/components/icons/printer-solid.svelte';
-import InkOutline from '$lib/components/icons/ink-outline.svelte';
-import Phone from '$lib/components/icons/phone.svelte';
-import HDDRack from '$lib/components/icons/hdd-rack.svelte';
-import ServerPathLinear from '$lib/components/icons/server-path-linear.svelte';
-import MapConnection from '$lib/components/icons/map-connection.svelte';
-import DeviceIpad from '$lib/components/icons/device-ipad.svelte';
-import Question24Filled from '$lib/components/icons/question-24-filled.svelte';
-import Cable from '$lib/components/icons/cable.svelte';
-import Sim24Regular from '$lib/components/icons/sim-24-regular.svelte';
-import GlobalStorageArchitecture from '$lib/components/icons/global-storage-architecture.svelte';
-import HeadsetSolid from '$lib/components/icons/headset-solid.svelte';
-import TaskListSquare24Regular from '$lib/components/icons/task-list-square-24-regular.svelte';
-import CRMService from '$lib/components/icons/crm-service.svelte';
-import Calendar from '$lib/components/icons/calendar.svelte';
-import ChartPie from '$lib/components/icons/chart-pie.svelte';
-import LucideWallet from '$lib/components/icons/lucide-wallet.svelte';
-import CodiconKey from '$lib/components/icons/codicon-key.svelte';
-import SolarCalculatorOutline from '$lib/components/icons/solar-calculator-outline.svelte';
-import WheelBarrowEmpty from '$lib/components/icons/wheel-barrow-empty.svelte';
-import ContactCardRegular from '$lib/components/icons/contact-card-regular.svelte';
-import ContractOutlineRounded from '$lib/components/icons/contract-outline-rounded.svelte';
-import SolarDocumentsOutline from '$lib/components/icons/solar-documents-outline.svelte';
-import UilPhone from '$lib/components/icons/uil-phone.svelte';
-import FluentCertificateRegular from '$lib/components/icons/fluent-certificate-regular.svelte';
-import BuildingFour from '$lib/components/icons/building-four.svelte';
-import CarbonAssemblyCluster from '$lib/components/icons/carbon-assembly-cluster.svelte';
-import StreamlinePlumpWeb from '$lib/components/icons/streamline-plump-web.svelte';
-import PhDevices from '$lib/components/icons/ph-devices.svelte';
-import StreamlinePlumpDatabase from '$lib/components/icons/streamline-plump-database.svelte';
-import IconoirTools from '$lib/components/icons/iconoir-tools.svelte';
-import TablerTemplate from '$lib/components/icons/tabler-template.svelte';
-import PhNotification from '$lib/components/icons/ph-notification.svelte';
-import MeteorIconsRss from '$lib/components/icons/meteor-icons-rss.svelte';
-import HugeiconsKnowledge from '$lib/components/icons/hugeicons-knowledge.svelte';
-import AkarIconsSchedule from '$lib/components/icons/akar-icons-schedule.svelte';
-import TablerReport from '$lib/components/icons/tabler-report.svelte';
-import FamiconsBookmarksOutline from '$lib/components/icons/famicons-bookmarks-outline.svelte';
-import TablerShieldCheck from '$lib/components/icons/tabler-shield-check.svelte';
-import TablerUser from '$lib/components/icons/tabler-user.svelte';
-import type { UserDetails, UserResponseData } from '$lib/types/api/auth';
+import type { NavGroup } from "$lib/types/navigation";
+import BxsDashboard from "$lib/components/icons/bxs-dashboard.svelte";
+import Package2Outline from "$lib/components/icons/package-2-outline.svelte";
+import ComputerOutline from "$lib/components/icons/computer-outline.svelte";
+import Monitor from "$lib/components/icons/monitor.svelte";
+import Software from "$lib/components/icons/software.svelte";
+import NetworkLeft from "$lib/components/icons/network-left.svelte";
+import USB from "$lib/components/icons/usb.svelte";
+import Printer from "$lib/components/icons/printer-solid.svelte";
+import InkOutline from "$lib/components/icons/ink-outline.svelte";
+import Phone from "$lib/components/icons/phone.svelte";
+import HDDRack from "$lib/components/icons/hdd-rack.svelte";
+import ServerPathLinear from "$lib/components/icons/server-path-linear.svelte";
+import MapConnection from "$lib/components/icons/map-connection.svelte";
+import DeviceIpad from "$lib/components/icons/device-ipad.svelte";
+import Question24Filled from "$lib/components/icons/question-24-filled.svelte";
+import Cable from "$lib/components/icons/cable.svelte";
+import Sim24Regular from "$lib/components/icons/sim-24-regular.svelte";
+import GlobalStorageArchitecture from "$lib/components/icons/global-storage-architecture.svelte";
+import HeadsetSolid from "$lib/components/icons/headset-solid.svelte";
+import TaskListSquare24Regular from "$lib/components/icons/task-list-square-24-regular.svelte";
+import CRMService from "$lib/components/icons/crm-service.svelte";
+import Calendar from "$lib/components/icons/calendar.svelte";
+import ChartPie from "$lib/components/icons/chart-pie.svelte";
+import LucideWallet from "$lib/components/icons/lucide-wallet.svelte";
+import CodiconKey from "$lib/components/icons/codicon-key.svelte";
+import SolarCalculatorOutline from "$lib/components/icons/solar-calculator-outline.svelte";
+import WheelBarrowEmpty from "$lib/components/icons/wheel-barrow-empty.svelte";
+import ContactCardRegular from "$lib/components/icons/contact-card-regular.svelte";
+import ContractOutlineRounded from "$lib/components/icons/contract-outline-rounded.svelte";
+import SolarDocumentsOutline from "$lib/components/icons/solar-documents-outline.svelte";
+import UilPhone from "$lib/components/icons/uil-phone.svelte";
+import FluentCertificateRegular from "$lib/components/icons/fluent-certificate-regular.svelte";
+import BuildingFour from "$lib/components/icons/building-four.svelte";
+import CarbonAssemblyCluster from "$lib/components/icons/carbon-assembly-cluster.svelte";
+import StreamlinePlumpWeb from "$lib/components/icons/streamline-plump-web.svelte";
+import PhDevices from "$lib/components/icons/ph-devices.svelte";
+import StreamlinePlumpDatabase from "$lib/components/icons/streamline-plump-database.svelte";
+import IconoirTools from "$lib/components/icons/iconoir-tools.svelte";
+import TablerTemplate from "$lib/components/icons/tabler-template.svelte";
+import PhNotification from "$lib/components/icons/ph-notification.svelte";
+import MeteorIconsRss from "$lib/components/icons/meteor-icons-rss.svelte";
+import HugeiconsKnowledge from "$lib/components/icons/hugeicons-knowledge.svelte";
+import AkarIconsSchedule from "$lib/components/icons/akar-icons-schedule.svelte";
+import TablerReport from "$lib/components/icons/tabler-report.svelte";
+import FamiconsBookmarksOutline from "$lib/components/icons/famicons-bookmarks-outline.svelte";
+import TablerShieldCheck from "$lib/components/icons/tabler-shield-check.svelte";
+import TablerUser from "$lib/components/icons/tabler-user.svelte";
+import type { UserDetails, UserResponseData } from "$lib/types/api/auth";
+import { getUserType } from "$lib/utils/users";
 
 export const APP_NAVIGATION: NavGroup[] = [
 	{
 		icon: Package2Outline,
-		label: 'Assets',
-		baseRoute: '/app/assets',
+		label: "Assets",
+		baseRoute: "/app/assets",
 		navs: [
 			{
-				label: 'Dashboard',
-				href: '/app/assets',
+				label: "Dashboard",
+				href: "/app/assets",
 				disabled: true,
-				icon: BxsDashboard
+				icon: BxsDashboard,
 			},
 			{
-				label: 'Computer',
-				href: '/app/assets',
+				label: "Computer",
+				href: "/app/assets",
 				disabled: true,
-				icon: ComputerOutline
+				icon: ComputerOutline,
 			},
 			{
-				label: 'Monitor',
-				href: '/app/assets',
+				label: "Monitor",
+				href: "/app/assets",
 				disabled: true,
-				icon: Monitor
+				icon: Monitor,
 			},
 			{
-				label: 'Software',
-				href: '/app/assets',
+				label: "Software",
+				href: "/app/assets",
 				disabled: true,
-				icon: Software
+				icon: Software,
 			},
 			{
-				label: 'Network Devices',
-				href: '/app/assets',
+				label: "Network Devices",
+				href: "/app/assets",
 				disabled: true,
-				icon: NetworkLeft
+				icon: NetworkLeft,
 			},
 			{
-				label: 'Peripherals',
-				href: '/app/assets',
+				label: "Peripherals",
+				href: "/app/assets",
 				disabled: true,
-				icon: USB
+				icon: USB,
 			},
 			{
-				label: 'Printers',
-				href: '/app/assets',
+				label: "Printers",
+				href: "/app/assets",
 				disabled: true,
-				icon: Printer
+				icon: Printer,
 			},
 			{
-				label: 'Cartridges',
-				href: '/app/assets',
+				label: "Cartridges",
+				href: "/app/assets",
 				disabled: true,
-				icon: InkOutline
+				icon: InkOutline,
 			},
 			{
-				label: 'Consumables',
-				href: '/app/assets',
+				label: "Consumables",
+				href: "/app/assets",
 				disabled: true,
-				icon: Package2Outline
+				icon: Package2Outline,
 			},
 			{
-				label: 'Phones',
-				href: '/app/assets',
+				label: "Phones",
+				href: "/app/assets",
 				disabled: true,
-				icon: Phone
+				icon: Phone,
 			},
 			{
-				label: 'Racks',
-				href: '/app/assets',
+				label: "Racks",
+				href: "/app/assets",
 				disabled: true,
-				icon: HDDRack
+				icon: HDDRack,
 			},
 			{
-				label: 'Enclosures',
-				href: '/app/assets',
+				label: "Enclosures",
+				href: "/app/assets",
 				disabled: true,
-				icon: ServerPathLinear
+				icon: ServerPathLinear,
 			},
 			{
-				label: 'PUCs',
-				href: '/app/assets',
+				label: "PUCs",
+				href: "/app/assets",
 				disabled: true,
-				icon: MapConnection
+				icon: MapConnection,
 			},
 			{
-				label: 'Passive Devices',
-				href: '/app/assets',
+				label: "Passive Devices",
+				href: "/app/assets",
 				disabled: true,
-				icon: DeviceIpad
+				icon: DeviceIpad,
 			},
 			{
-				label: 'Unmanaged Devices',
-				href: '/app/assets',
+				label: "Unmanaged Devices",
+				href: "/app/assets",
 				disabled: true,
-				icon: Question24Filled
+				icon: Question24Filled,
 			},
 			{
-				label: 'Cables',
-				href: '/app/assets',
+				label: "Cables",
+				href: "/app/assets",
 				disabled: true,
-				icon: Cable
+				icon: Cable,
 			},
 			{
-				label: 'Simcard Items',
-				href: '/app/assets',
+				label: "Simcard Items",
+				href: "/app/assets",
 				disabled: true,
-				icon: Sim24Regular
+				icon: Sim24Regular,
 			},
 			{
-				label: 'Global',
-				href: '/app/assets',
+				label: "Global",
+				href: "/app/assets",
 				disabled: true,
-				icon: GlobalStorageArchitecture
-			}
-		]
+				icon: GlobalStorageArchitecture,
+			},
+		],
 	},
 	{
 		icon: HeadsetSolid,
-		label: 'Assistance',
-		baseRoute: '/app/assistance',
+		label: "Assistance",
+		baseRoute: "/app/assistance",
 		navs: [
 			{
-				label: 'Dashboard',
-				href: '/app/assistance/dashboard',
+				label: "Dashboard",
+				href: "/app/assistance/dashboard",
 				disabled: false,
-				icon: BxsDashboard
+				icon: BxsDashboard,
 			},
 			{
-				label: 'Tickets',
-				href: '/app/assistance/tickets',
+				label: "Tickets",
+				href: "/app/assistance/tickets",
 				disabled: false,
-				icon: TaskListSquare24Regular
+				icon: TaskListSquare24Regular,
 			},
 			{
-				label: 'Services',
-				href: '/app/assistance/services',
+				label: "Services",
+				href: "/app/assistance/services",
 				disabled: true,
-				icon: CRMService
+				icon: CRMService,
 			},
 			{
-				label: 'Planning',
-				href: '/app/assistance/planning',
+				label: "Planning",
+				href: "/app/assistance/planning",
 				disabled: true,
-				icon: Calendar
+				icon: Calendar,
 			},
 			{
-				label: 'Statistics',
-				href: '/app/assistance/statistics',
+				label: "Statistics",
+				href: "/app/assistance/statistics",
 				disabled: true,
-				icon: ChartPie
-			}
-		]
+				icon: ChartPie,
+			},
+		],
 	},
 	{
 		icon: LucideWallet,
-		label: 'Management',
-		baseRoute: '/app/management',
+		label: "Management",
+		baseRoute: "/app/management",
 		navs: [
 			{
-				label: 'Licences',
-				href: '/app/management',
+				label: "Licences",
+				href: "/app/management",
 				disabled: true,
-				icon: CodiconKey
+				icon: CodiconKey,
 			},
 			{
-				label: 'Budgets',
-				href: '/app/management',
+				label: "Budgets",
+				href: "/app/management",
 				disabled: true,
-				icon: SolarCalculatorOutline
+				icon: SolarCalculatorOutline,
 			},
 			{
-				label: 'Suppliers',
-				href: '/app/management',
+				label: "Suppliers",
+				href: "/app/management",
 				disabled: true,
-				icon: WheelBarrowEmpty
+				icon: WheelBarrowEmpty,
 			},
 			{
-				label: 'Contacts',
-				href: '/app/management',
+				label: "Contacts",
+				href: "/app/management",
 				disabled: true,
-				icon: ContactCardRegular
+				icon: ContactCardRegular,
 			},
 			{
-				label: 'Contracts',
-				href: '/app/management',
+				label: "Contracts",
+				href: "/app/management",
 				disabled: true,
-				icon: ContractOutlineRounded
+				icon: ContractOutlineRounded,
 			},
 			{
-				label: 'Documents',
-				href: '/app/management',
+				label: "Documents",
+				href: "/app/management",
 				disabled: true,
-				icon: SolarDocumentsOutline
+				icon: SolarDocumentsOutline,
 			},
 			{
-				label: 'Lines',
-				href: '/app/management',
+				label: "Lines",
+				href: "/app/management",
 				disabled: true,
-				icon: UilPhone
+				icon: UilPhone,
 			},
 			{
-				label: 'Certificates',
-				href: '/app/management',
+				label: "Certificates",
+				href: "/app/management",
 				disabled: true,
-				icon: FluentCertificateRegular
+				icon: FluentCertificateRegular,
 			},
 			{
-				label: 'Datacenters',
-				href: '/app/management',
+				label: "Datacenters",
+				href: "/app/management",
 				disabled: true,
-				icon: BuildingFour
+				icon: BuildingFour,
 			},
 			{
-				label: 'Clusters',
-				href: '/app/management',
+				label: "Clusters",
+				href: "/app/management",
 				disabled: true,
-				icon: CarbonAssemblyCluster
+				icon: CarbonAssemblyCluster,
 			},
 			{
-				label: 'Domains',
-				href: '/app/management',
+				label: "Domains",
+				href: "/app/management",
 				disabled: true,
-				icon: StreamlinePlumpWeb
+				icon: StreamlinePlumpWeb,
 			},
 			{
-				label: 'Appliances',
-				href: '/app/management',
+				label: "Appliances",
+				href: "/app/management",
 				disabled: true,
-				icon: PhDevices
+				icon: PhDevices,
 			},
 			{
-				label: 'Databases',
-				href: '/app/management',
+				label: "Databases",
+				href: "/app/management",
 				disabled: true,
-				icon: StreamlinePlumpDatabase
-			}
-		]
+				icon: StreamlinePlumpDatabase,
+			},
+		],
 	},
 	{
 		icon: IconoirTools,
-		label: 'Tools',
-		baseRoute: '/app/management',
+		label: "Tools",
+		baseRoute: "/app/management",
 		navs: [
 			{
-				label: 'Project',
-				href: '/app/tools',
+				label: "Project",
+				href: "/app/tools",
 				disabled: true,
-				icon: TablerTemplate
+				icon: TablerTemplate,
 			},
 			{
-				label: 'Reminders',
-				href: '/app/tools',
+				label: "Reminders",
+				href: "/app/tools",
 				disabled: true,
-				icon: PhNotification
+				icon: PhNotification,
 			},
 			{
-				label: 'RSS Feed',
-				href: '/app/tools',
+				label: "RSS Feed",
+				href: "/app/tools",
 				disabled: true,
-				icon: MeteorIconsRss
+				icon: MeteorIconsRss,
 			},
 			{
-				label: 'Knowledge Base',
-				href: '/app/tools',
+				label: "Knowledge Base",
+				href: "/app/tools",
 				disabled: true,
-				icon: HugeiconsKnowledge
+				icon: HugeiconsKnowledge,
 			},
 			{
-				label: 'Reservations',
-				href: '/app/tools',
+				label: "Reservations",
+				href: "/app/tools",
 				disabled: true,
-				icon: AkarIconsSchedule
+				icon: AkarIconsSchedule,
 			},
 			{
-				label: 'Reports',
-				href: '/app/tools',
+				label: "Reports",
+				href: "/app/tools",
 				disabled: true,
-				icon: TablerReport
+				icon: TablerReport,
 			},
 			{
-				label: 'Saved Searches',
-				href: '/app/tools',
+				label: "Saved Searches",
+				href: "/app/tools",
 				disabled: true,
-				icon: FamiconsBookmarksOutline
-			}
-		]
+				icon: FamiconsBookmarksOutline,
+			},
+		],
 	},
 	{
 		icon: TablerShieldCheck,
-		label: 'Administration',
-		baseRoute: '/app/administration',
+		label: "Administration",
+		baseRoute: "/app/administration",
 		navs: [
 			{
-				label: 'Users',
-				href: '/app/administration',
+				label: "Users",
+				href: "/app/administration",
 				disabled: true,
-				icon: TablerUser
-			}
-		]
-	}
+				icon: TablerUser,
+			},
+		],
+	},
 ];
 
 export function getUserNaviation(user: UserResponseData) {
-	if (!user.user.is_staff && !user.groups && !user.queues) {
-		return APP_NAVIGATION.filter((nav) => ['Assistance'].includes(nav.label));
+	const userType = getUserType(user);
+	if (userType === "user") {
+		return APP_NAVIGATION.filter((nav) => ["Assistance"].includes(nav.label));
 	}
 	return APP_NAVIGATION;
 }
