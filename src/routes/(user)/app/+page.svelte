27e88493951b0d34537c2e04a69551b04a4112db
Ezekiel Victor <codeepoch@gmail.com>
2025-10-21 16:04:25 +0100

    made admin-dashboard page

--- src/routes/(user)/app/+page.svelte
+++ src/routes/(user)/app/+page.svelte
@@ -2,14 +2,17 @@
 	import { getUserType } from '$lib/utils/users.js';
 	import StaffAssisntanceDashboard from '$lib/components/pages/staff-assisntance-dashboard.svelte';
 	import UserAssistanceDashboard from '$lib/components/pages/user-assistance-dashboard.svelte';
+	import AdminDashboard from '$lib/components/pages/admin-dashboard.svelte';
 
 	let { data } = $props();
 	const userType = getUserType(data.user);
 </script>
 
 <div class="h-[calc(100vh-70px)] overflow-y-auto">
-	{#if userType !== 'user'}
+	{#if userType === 'staff'}
 		<StaffAssisntanceDashboard user={data.user} />
+	{:else if userType === 'admin'}
+		<AdminDashboard />
 	{:else}
 		<UserAssistanceDashboard />
 	{/if}
