b36259c5bb97485871715f8dd97ec75d3f4365c3
Ezekiel Victor <codeepoch@gmail.com>
2025-11-03 07:53:38 +0100

    Feat: update backend url

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: "https://itmsmvp-81072574537.europe-west1.run.app/swagger/?format=openapi",
-  output: "src/lib/services/api",
+	input: "https://itms-mvp.onrender.com/swagger/?format=openapi",
+	output: "src/lib/services/api",
 });
